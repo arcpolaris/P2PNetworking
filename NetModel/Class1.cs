b1af426babbb62a8c0f683e79bdcfb4b87a9f1d5
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2026-03-13 17:14:40 -0700

    Add project files.

---
+++ NetModel/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetModel;
+
+public static class Class1
+{
+	public static int Square(int x) => x * x;
+}
---
+++ TestEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestEngine;
+
+using NetModel;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine(Class1.Square(2));
+	}
+}
