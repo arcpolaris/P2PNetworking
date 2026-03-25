0c582aaa1578534e2d3d2f4d3bdc577043489385
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2026-03-25 12:41:52 -0700

    Source Generation Works, now to implement it

---
+++ NetModel/ProtocolAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetModel;
+
+[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+public class ProtocolAttribute : Attribute
+{
+	public ProtocolAttribute()
+	{
+
+	}
+}
---
+++ TestEngine/CodeGenTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetModel;
+
+namespace TestEngine;
+
+[Protocol]
+public partial interface ITestProtocol
+{
+
+}
+
+[TestClass]
+public sealed class CodeGenTest
+{
+	
+
+	[TestMethod]
+	public void CodeGenSanity()
+	{
+		ITestProtocol.Test();
+	}
+}
